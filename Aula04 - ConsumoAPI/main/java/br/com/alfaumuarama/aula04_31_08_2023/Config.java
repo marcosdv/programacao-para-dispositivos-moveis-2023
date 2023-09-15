76c7532b399e575eef0a3044ad1773cbfbf86603
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-09-14 22:01:27 -0300

    Delete Aula04 - ConsumoAPI/main directory

--- Aula04 - ConsumoAPI/main/java/br/com/alfaumuarama/aula04_31_08_2023/Config.java
+++
@@ -1,6 +0,0 @@
-package br.com.alfaumuarama.aula04_31_08_2023;
-
-public class Config {
-    public static String linkServer = "https://pokeapi.co/api/v2/pokemon";
-    public static String link = linkServer + "?limit=151";
-}
