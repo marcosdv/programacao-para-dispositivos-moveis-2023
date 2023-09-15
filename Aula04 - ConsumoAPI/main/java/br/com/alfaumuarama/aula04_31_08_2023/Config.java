fc98868f303026418491966fa70c0ce046ac126f
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2023-09-14 22:16:42 -0300

    Aula05 - 14-09-2023

---
+++ Aula04 - ConsumoAPI/main/java/br/com/alfaumuarama/aula04_31_08_2023/Config.java
@@ -0,0 +1,6 @@
+package br.com.alfaumuarama.aula04_31_08_2023;
+
+public class Config {
+    public static String linkServer = "https://pokeapi.co/api/v2/pokemon";
+    public static String link = linkServer + "?limit=151";
+}
